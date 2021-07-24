782311a5aee29ee899a58050a6ed63562ae8c691
nikolaslacerda <nikolas.lacerda@hotmail.com>
2021-07-24 16:44:29 -0300

    add create and find all products endpoints

--- microservices/product-service/src/main/java/com/server/productservice/product/service/ProductService.java
+++ microservices/product-service/src/main/java/com/server/productservice/product/service/ProductService.java
@@ -1,7 +1,23 @@
 package com.server.productservice.product.service;
 
+import com.server.productservice.product.entity.Product;
+import com.server.productservice.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
 }
