2589f634908075780be93b8aad53905ed7e850d2
Nikolas Lacerda <nikolas.lacerda@hotmail.com>
2021-11-20 15:40:20 -0300

    add api gateway service

---
+++ microservices/api-gateway-service/src/test/java/com/server/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
